25c9690984df597c1320065ca8dbe776552562c7
bpior <bartlomiejpiorczynski@gmail.com>
2021-12-19 13:12:58 +0100

    Initial commit

---
+++ testoo/Program.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+for (int i = 0; i < 100_000_000; i++) { Consume(CryptoConfig.CreateFromName("RSA")); }
+[MethodImpl(MethodImplOptions.NoInlining)] static void Consume<T>(in T _) { }
